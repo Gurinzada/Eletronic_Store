163b352e4cc83cd8d6384afa509117b9ce17de45
Gurinzada <augustoinacio243@gmail.com>
2024-05-31 15:16:19 -0300

    FormatingTheCodes

--- src/Pages/Cart.tsx
+++ src/Pages/Cart.tsx
@@ -1,101 +1,123 @@
-import { useEffect, useState } from "react"
-import { Link, useParams } from "react-router-dom"
-import { User, getDataById } from "../services/user"
-import { Bounce, ToastContainer, toast } from "react-toastify"
-import 'react-toastify/ReactToastify.css';
-import style from "../Styles/Cart.module.scss"
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { User, getDataById } from "../services/user";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import "react-toastify/ReactToastify.css";
+import style from "../Styles/Cart.module.scss";
 
-export default function Cart(){
-    const {UserID} = useParams()
-    const [infosUser, setInfosUser] = useState<User | null>(null)
-    const [TheTotal, setTheTotal] = useState<String>(``)
-    const [changeState, setChangeState] = useState(false)
+export default function Cart() {
+  const { UserID } = useParams();
+  const [infosUser, setInfosUser] = useState<User | null>(null);
+  const [TheTotal, setTheTotal] = useState<String>(``);
+  const [changeState, setChangeState] = useState(false);
 
-    useEffect(() => {
-        getDataById(UserID).then((res:User) => setInfosUser(res))
-        theTotal()
-        console.log(infosUser)
-        setChangeState(false)
-    },[changeState])
+  useEffect(() => {
+    getDataById(UserID).then((res: User) => setInfosUser(res));
+    theTotal();
+    console.log(infosUser);
+    setChangeState(false);
+  }, [changeState]);
 
-    const theTotal = () => {
-        const total = infosUser?.Cart.reduce((actual, current) => actual + current.price, 0 )
-         setTheTotal(String(total?.toFixed(2)))
+  const theTotal = () => {
+    const total = infosUser?.Cart.reduce(
+      (actual, current) => actual + current.price,
+      0
+    );
+    setTheTotal(String(total?.toFixed(2)));
+  };
+
+  useEffect(() => {
+    theTotal();
+  }, [infosUser]);
+
+  const removeProduct = async (indexProduct: number) => {
+    if (infosUser) {
+      const updatedCart = infosUser.Cart.filter(
+        (_product, index) => index !== indexProduct
+      );
+      const updatedInfos = { ...infosUser, Cart: updatedCart };
+      toast.success(`Produto removido com sucesso!`, {
+        position: "bottom-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
+      await fetch(`${import.meta.env.VITE_PUBLIC_APIURL}/${UserID}`, {
+        method: "PUT",
+        body: JSON.stringify(updatedInfos),
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      });
+      setInfosUser(updatedInfos);
+      console.log(updatedInfos);
+      setChangeState(true);
     }
+  };
 
-    useEffect(() => {
-        theTotal()
-    },[infosUser])
-    
-    const removeProduct = async(indexProduct:number) => {
-            if(infosUser){
-                const updatedCart = infosUser.Cart.filter((_product,index) => index !== indexProduct)
-                const updatedInfos = {...infosUser, Cart:updatedCart}
-                toast.success(`Produto removido com sucesso!`, {
-                    position: "bottom-right",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                    transition: Bounce,
-                    });
-            await fetch(`${import.meta.env.VITE_PUBLIC_APIURL}/${UserID}`,{
-                method:"PUT",
-                body: JSON.stringify(updatedInfos),
-                headers:{
-                    "Content-Type": "Application/json"
-                }
-            })
-            setInfosUser(updatedInfos)
-            console.log(updatedInfos)
-            setChangeState(true)
-        }
-    }
-
-    return(
-        <div className={style.WrapperCart}>
-            <header className={style.HeaderCart}>
-                <div>
-                    <h1>Bem vindo ao seu carrinho {infosUser?.FirstName}</h1>
-                    <p>Pronto para finalizar sua compra?</p>
-                </div>
-                <nav>
-                    <Link to={`/commercial/${UserID}`}><button className={style.Bnt}>Voltar</button></Link>
-                </nav>
-            </header>
-            <main>
-                <section className={style.SectionTable}>
-                    <h3 className={style.TitleH3Cart}>Aqui estão suas compras!</h3>
-                    <table >
-                        <thead>
-                            <tr>
-                                <th scope="col"><h1>Produto</h1></th>
-                                <th scope="col"><h1>Preço</h1></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                    {infosUser?.Cart.length || undefined ?  infosUser?.Cart.map((user, index) =>(
-                       <tr key={index} >
-                        <th scope="row">{user.name}</th>
-                        <th>R$ {user.price}</th>
-                        <th  style={{textAlign:"center"}}><button onClick={() => removeProduct(index)} className={style.BntRemove} style={{color:"white"}}>-</button></th>
-                       </tr>
-                    )): <></> }
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th scope="row">Valor Total</th>
-                                <td>R$ {TheTotal === undefined ? 0 : TheTotal}</td>
-                            </tr>
-                        </tfoot>
-                    </table>
-                    <ToastContainer/>
-                </section>
-            </main>
+  return (
+    <div className={style.WrapperCart}>
+      <header className={style.HeaderCart}>
+        <div>
+          <h1>Bem vindo ao seu carrinho {infosUser?.FirstName}</h1>
+          <p>Pronto para finalizar sua compra?</p>
         </div>
-        
-    )
+        <nav>
+          <Link to={`/commercial/${UserID}`}>
+            <button className={style.Bnt}>Voltar</button>
+          </Link>
+        </nav>
+      </header>
+      <main>
+        <section className={style.SectionTable}>
+          <h3 className={style.TitleH3Cart}>Aqui estão suas compras!</h3>
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">
+                  <h1>Produto</h1>
+                </th>
+                <th scope="col">
+                  <h1>Preço</h1>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {infosUser?.Cart.length || undefined ? (
+                infosUser?.Cart.map((user, index) => (
+                  <tr key={index}>
+                    <th scope="row">{user.name}</th>
+                    <th>R$ {user.price}</th>
+                    <th style={{ textAlign: "center" }}>
+                      <button
+                        onClick={() => removeProduct(index)}
+                        className={style.BntRemove}
+                        style={{ color: "white" }}
+                      >
+                        -
+                      </button>
+                    </th>
+                  </tr>
+                ))
+              ) : (
+                <></>
+              )}
+            </tbody>
+            <tfoot>
+              <tr>
+                <th scope="row">Valor Total</th>
+                <td>R$ {TheTotal === undefined ? 0 : TheTotal}</td>
+              </tr>
+            </tfoot>
+          </table>
+          <ToastContainer />
+        </section>
+      </main>
+    </div>
+  );
 }
--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -2,32 +2,37 @@ import { Link, useParams } from "react-router-dom";
 import style from "../Styles/Commercial.module.scss";
 import { useAuth } from "../context/AuthProvider";
 import { useEffect, useState } from "react";
-import { Address, CheckCep, CheckPassword, User, getDataById } from "../services/user";
+import {
+  Address,
+  CheckCep,
+  CheckPassword,
+  User,
+  getDataById,
+} from "../services/user";
 import DefaultFooter from "../components/DefaultFooter";
 import { Bounce, ToastContainer, toast } from "react-toastify";
-import 'react-toastify/ReactToastify.css';
-import styleProfile from "../Styles/Profiles.module.scss"
+import "react-toastify/ReactToastify.css";
+import styleProfile from "../Styles/Profiles.module.scss";
 
 export default function Profile() {
   const [infosUser, setInfosUser] = useState<User | null>(null);
-  const [street, setStreet] = useState<string>(``)
-  const [neighborhood, setNeighborhood] = useState<string>(``)
-  const [houseNumber, setHouseNumber] = useState<number>(0)
-  const [cep, setCep] = useState<string>(``)
-  const [change, setChange] = useState<boolean>(false)
-  const [WrongCep, setWrongCep] = useState<boolean>(false)
-  const [password, setPassword] = useState(``)
-  const [passwordConfirm, setPasswordConfirm] = useState(``)
+  const [street, setStreet] = useState<string>(``);
+  const [neighborhood, setNeighborhood] = useState<string>(``);
+  const [houseNumber, setHouseNumber] = useState<number>(0);
+  const [cep, setCep] = useState<string>(``);
+  const [change, setChange] = useState<boolean>(false);
+  const [WrongCep, setWrongCep] = useState<boolean>(false);
+  const [password, setPassword] = useState(``);
+  const [passwordConfirm, setPasswordConfirm] = useState(``);
   const { logout } = useAuth();
   const { UserID } = useParams();
 
-
   const cepIsWrong = () => {
-    setWrongCep(true)
+    setWrongCep(true);
     setTimeout(() => {
-      setWrongCep(false)
-    },3000)
-  }
+      setWrongCep(false);
+    }, 3000);
+  };
 
   const handleLogout = () => {
     logout();
@@ -40,15 +45,15 @@ export default function Profile() {
 
   useEffect(() => {
     datas().then((res: User) => setInfosUser(res));
-    setChange(false)
-  },[change]);
+    setChange(false);
+  }, [change]);
 
-  const handleChangesPassword = async(e:React.FormEvent) => {
-    e.preventDefault()
-    const passwordIsRigth = CheckPassword(password)
-    if(passwordIsRigth && password === passwordConfirm && infosUser){
-      const updateInfos = {...infosUser}
-      updateInfos.password = password
+  const handleChangesPassword = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const passwordIsRigth = CheckPassword(password);
+    if (passwordIsRigth && password === passwordConfirm && infosUser) {
+      const updateInfos = { ...infosUser };
+      updateInfos.password = password;
       toast.success(`Senha atualizada com sucesso! Aproveite 😊`, {
         position: "bottom-right",
         autoClose: 2000,
@@ -58,32 +63,31 @@ export default function Profile() {
         draggable: true,
         theme: "light",
         transition: Bounce,
-      })
+      });
 
-      await fetch(`${import.meta.env.VITE_PUBLIC_APIURL}/${UserID}`,{
-            method:"PUT",
-            body: JSON.stringify(updateInfos),
-            headers:{
-                "Content-Type": "Application/json"
-            }
-        })
+      await fetch(`${import.meta.env.VITE_PUBLIC_APIURL}/${UserID}`, {
+        method: "PUT",
+        body: JSON.stringify(updateInfos),
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      });
 
-        setInfosUser(updateInfos)
-        setChange(true)
-        setPassword(``)
-        setPasswordConfirm(``)
-    } else{
-      setPassword(``)
-        setPasswordConfirm(``)
-      throw new Error(`An error has ocurred!`)
-        
+      setInfosUser(updateInfos);
+      setChange(true);
+      setPassword(``);
+      setPasswordConfirm(``);
+    } else {
+      setPassword(``);
+      setPasswordConfirm(``);
+      throw new Error(`An error has ocurred!`);
     }
-  }
+  };
 
-  const handleAddress = async(e:React.FormEvent) => {
-    e.preventDefault()
-    const checkCep = CheckCep(cep)
-    if(infosUser && checkCep){
+  const handleAddress = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const checkCep = CheckCep(cep);
+    if (infosUser && checkCep) {
       toast.success(`Endereço ataulizado com sucesso! Aproveite 😊`, {
         position: "bottom-right",
         autoClose: 2000,
@@ -93,35 +97,35 @@ export default function Profile() {
         draggable: true,
         theme: "light",
         transition: Bounce,
-      })
-        const updatedInfos = {...infosUser}
-        const myNewAddress:Address ={
-            Street: street,
-            cep: cep,
-            HouseNumber:houseNumber,
-            Neighborhood:neighborhood
-        }
-        updatedInfos.Address[0] = myNewAddress
+      });
+      const updatedInfos = { ...infosUser };
+      const myNewAddress: Address = {
+        Street: street,
+        cep: cep,
+        HouseNumber: houseNumber,
+        Neighborhood: neighborhood,
+      };
+      updatedInfos.Address[0] = myNewAddress;
 
-        await fetch(`${import.meta.env.VITE_PUBLIC_APIURL}/${UserID}`,{
-            method:"PUT",
-            body: JSON.stringify(updatedInfos),
-            headers:{
-                "Content-Type": "Application/json"
-            }
-        })
+      await fetch(`${import.meta.env.VITE_PUBLIC_APIURL}/${UserID}`, {
+        method: "PUT",
+        body: JSON.stringify(updatedInfos),
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      });
 
-        setInfosUser(updatedInfos)
-        setCep(``)
-        setHouseNumber(0)
-        setNeighborhood(``)
-        setStreet(``)
-        setChange(true)
-    } else{
-      cepIsWrong()
-      throw new Error(`Model of Cep is invalid`)
+      setInfosUser(updatedInfos);
+      setCep(``);
+      setHouseNumber(0);
+      setNeighborhood(``);
+      setStreet(``);
+      setChange(true);
+    } else {
+      cepIsWrong();
+      throw new Error(`Model of Cep is invalid`);
     }
-  }
+  };
 
   return (
     <div className={styleProfile.WrapperProfile}>
@@ -146,44 +150,89 @@ export default function Profile() {
       </header>
       <main className={styleProfile.ContentForms}>
         <form onSubmit={handleAddress} className={styleProfile.Forms}>
-                <h3>Endereço</h3>
-                <div className={styleProfile.ContentForm}>
-                    <label htmlFor="Street">Rua</label>
-                    <input type="text" id="Street" required value={street} onChange={(e) => setStreet(e.target.value)} placeholder={`Rua | Av XXXXX`}/>
-                </div>
-                <div className={styleProfile.ContentForm}>
-                    <label htmlFor="Neighborhood">Bairro</label>
-                    <input type="text" id="Neighborhood" value={neighborhood} onChange={(e) => setNeighborhood(e.target.value)} required/>
-                </div>
-                <div className={styleProfile.ContentForm}>
-                    <label htmlFor="HouseNumber">Número</label>
-                    <input type="number" id="HouseNumber" value={houseNumber} onChange={(e) => setHouseNumber(Number(e.target.value))} required/>
-                </div>
-                <div className={styleProfile.ContentForm}>
-                    <label htmlFor="CEP">CEP</label>
-                    <input id="CEP" required maxLength={9} minLength={9} value={cep} onChange={(e) => setCep(e.target.value)} placeholder={`Ex: 12345-678`} />
-                    {WrongCep === true ? <span style={{color: "red"}}>Digite o modelo correto: XXXXX-XXX</span>: null}
-                </div>
-            
-            <button>Atualizar Informações</button>
+          <h3>Endereço</h3>
+          <div className={styleProfile.ContentForm}>
+            <label htmlFor="Street">Rua</label>
+            <input
+              type="text"
+              id="Street"
+              required
+              value={street}
+              onChange={(e) => setStreet(e.target.value)}
+              placeholder={`Rua | Av XXXXX`}
+            />
+          </div>
+          <div className={styleProfile.ContentForm}>
+            <label htmlFor="Neighborhood">Bairro</label>
+            <input
+              type="text"
+              id="Neighborhood"
+              value={neighborhood}
+              onChange={(e) => setNeighborhood(e.target.value)}
+              required
+            />
+          </div>
+          <div className={styleProfile.ContentForm}>
+            <label htmlFor="HouseNumber">Número</label>
+            <input
+              type="number"
+              id="HouseNumber"
+              value={houseNumber}
+              onChange={(e) => setHouseNumber(Number(e.target.value))}
+              required
+            />
+          </div>
+          <div className={styleProfile.ContentForm}>
+            <label htmlFor="CEP">CEP</label>
+            <input
+              id="CEP"
+              required
+              maxLength={9}
+              minLength={9}
+              value={cep}
+              onChange={(e) => setCep(e.target.value)}
+              placeholder={`Ex: 12345-678`}
+            />
+            {WrongCep === true ? (
+              <span style={{ color: "red" }}>
+                Digite o modelo correto: XXXXX-XXX
+              </span>
+            ) : null}
+          </div>
+
+          <button>Atualizar Informações</button>
         </form>
         <form onSubmit={handleChangesPassword} className={styleProfile.Forms}>
           <h3>Dados pessoais</h3>
           <div className={styleProfile.ContentForm}>
             <label htmlFor="UserName">Nome:</label>
-            <input disabled value={`${infosUser?.FirstName} ${infosUser?.SecondName}`} id="UserName"/>
+            <input
+              disabled
+              value={`${infosUser?.FirstName} ${infosUser?.SecondName}`}
+              id="UserName"
+            />
           </div>
           <div className={styleProfile.ContentForm}>
             <label htmlFor="EmailUser">Email:</label>
-            <input disabled value={infosUser?.email} id="EmailUser"/>
+            <input disabled value={infosUser?.email} id="EmailUser" />
           </div>
           <div className={styleProfile.ContentForm}>
             <label htmlFor="Password">Senha:</label>
-            <input type="password" id="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <input
+              type="password"
+              id="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className={styleProfile.ContentForm}>
             <label htmlFor="PasswordConfirm">Confirme sua senha:</label>
-            <input type="password" id="PasswordConfirm" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)}/>
+            <input
+              type="password"
+              id="PasswordConfirm"
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+            />
           </div>
           <div>
             <button>Atualizar Informações</button>
@@ -191,7 +240,7 @@ export default function Profile() {
         </form>
       </main>
       <ToastContainer />
-      <DefaultFooter/>
+      <DefaultFooter />
     </div>
   );
 }
